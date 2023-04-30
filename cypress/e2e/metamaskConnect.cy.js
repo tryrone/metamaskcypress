2924c7401a875a8ca69d1aec4b0b4a90cbf2321e
tryrone <tegararuvwe@gmail.com>
2023-04-30 16:14:28 +0100

    working on cypress matamask test suite

--- cypress/e2e/metamaskConnect.cy.js
+++ cypress/e2e/metamaskConnect.cy.js
@@ -1,18 +1,82 @@
 /* eslint-disable no-undef */
-// import { cy } from "cypress";
 
-describe("MetaMaskConnect component", () => {
-  it("connects to MetaMask and displays the user address", () => {
+// describe("App", () => {
+//   it("should connect to Metamask and display the address", () => {
+//     cy.visit("/");
+
+//     // Click the "connect" button
+//     cy.get("[data-cy=connect]").click();
+
+//     // Wait for Metamask to connect
+//     cy.window().should("have.property", "ethereum");
+//     cy.window().its("ethereum.selectedAddress").should("not.be.empty");
+
+//     // Get the address and check that it is displayed on the screen
+//     cy.window().then((win) => {
+//       const address = win.ethereum.selectedAddress;
+//       cy.get("[data-cy=address]").should("contain", address);
+//     });
+//   });
+// });
+
+// describe("App", () => {
+//   it("should connect to Metamask and display the address", () => {
+//     cy.visit("/");
+
+//     // Click the "connect" button
+//     cy.get("[data-cy=connect]").click();
+
+//     // Wait for Metamask to connect
+//     cy.window().should("have.property", "ethereum");
+//     cy.window().its("ethereum.selectedAddress").should("not.be.empty");
+
+//     // Get the address and check that it is displayed on the screen
+//     cy.window().then((win) => {
+//       const address = win.ethereum.selectedAddress;
+//       cy.get("[data-cy=address]").should("contain", address);
+
+//       // Switch to the Metamask window
+//       cy.visit(
+//         `chrome-extension://${Cypress.env("METAMASK_EXTENSION_ID")}/popup.html`
+//       );
+
+//       // Wait for the Metamask popup to load and the address to be displayed
+//       cy.get("#app-content").should("contain", address);
+
+//       // Click on the address to select it
+//       cy.get(".selected-account").click();
+
+//       // Switch back to the app window
+//       cy.visit("/");
+//     });
+//   });
+// });
+
+describe("App", () => {
+  it("should connect to Metamask and display the address", () => {
     cy.visit("/");
 
-    cy.get("button").contains("Connect MetaMask").click();
+    // Click the "connect" button
+    cy.get("[data-cy=connect]").click();
 
-    cy.wait(5000).then(() => {
-      // do something after waiting
-    }); // Wait for MetaMask to connect
+    // Wait for Metamask to connect
+    cy.window().should("have.property", "ethereum");
+    cy.window().its("ethereum.selectedAddress").should("not.be.empty");
 
-    cy.get("p")
-      .contains(/^Connected address: /)
-      .should("exist");
+    // Get the address and check that it is displayed on the screen
+    cy.window().then((win) => {
+      const address = win.ethereum.selectedAddress;
+      cy.get("[data-cy=address]").should("contain", address);
+
+      // Switch to the Metamask window
+      cy.visit(
+        `chrome-extension://${Cypress.env("METAMASK_EXTENSION_ID")}/popup.html`
+      );
+
+      // Switch back to the app window
+      cy.visit("/");
+
+      cy.get("[data-cy=address]").should("not.have.text", "-");
+    });
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,49 +1,93 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useState, useEffect, useCallback } from "react";
 import detectEthereumProvider from "@metamask/detect-provider";
+import { Web3Provider } from "@ethersproject/providers";
+import "./App.css";
 
-function App() {
-  const [address, setAddress] = useState("");
-
-  async function connectToMetaMask() {
-    try {
-      const provider = await detectEthereumProvider();
-      if (provider) {
-        await provider.request({ method: "eth_requestAccounts" });
-        setAddress(provider.selectedAddress);
+function requestAccess(ethereum, handleAccountsChanged) {
+  ethereum
+    //@ts-ignore
+    .request({ method: "eth_requestAccounts" })
+    .then(handleAccountsChanged)
+    .catch((err) => {
+      console.log("metamask request access");
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.log("Please connect to MetaMask.");
       } else {
-        console.log("MetaMask not found");
+        console.error(err);
       }
-    } catch (error) {
-      console.log(error);
+    });
+}
+
+export default function App() {
+  const [balance, setBalance] = useState("");
+  const [address, setAddress] = useState("");
+  const [provider, setProvider] = useState();
+  const hasWindow = typeof window !== "undefined";
+
+  const handleAccountsChanged = useCallback(
+    async (accounts) => {
+      if (accounts.length === 0) {
+        setBalance("");
+        setAddress("");
+      } else if (accounts[0] !== address) {
+        if (window.ethereum) {
+          const web3Provider = new Web3Provider(window.ethereum);
+          const newBalance = await web3Provider.getBalance(accounts[0]);
+          setAddress(accounts[0]);
+          setBalance(newBalance.toString());
+        }
+      }
+    },
+    [address]
+  );
+  const connect = useCallback(() => {
+    if (window.ethereum) {
+      requestAccess(window.ethereum, handleAccountsChanged);
     }
-  }
+  }, [handleAccountsChanged]);
 
   useEffect(() => {
-    async function checkMetaMask() {
-      const provider = await detectEthereumProvider();
-      if (provider) {
-        // Handle connection status changes
-        provider.on("accountsChanged", () => {
-          setAddress(provider.selectedAddress);
-        });
-      } else {
-        console.log("MetaMask not found");
+    const init = async () => {
+      if (hasWindow) {
+        const prov = await detectEthereumProvider();
+        setProvider(prov);
+        if (window.ethereum) {
+          //@ts-ignore
+          window.ethereum.on("accountsChanged", handleAccountsChanged);
+        }
       }
-    }
-    checkMetaMask();
-  }, []);
+    };
+    init();
+    return () => {
+      if (window && window.ethereum) {
+        //@ts-ignore
+        window.ethereum.removeListener(
+          "accountsChanged",
+          handleAccountsChanged
+        );
+      }
+    };
+  }, [handleAccountsChanged, hasWindow]);
+
   return (
-    <div className="App">
-      {address ? (
-        <p>Connected address: {address}</p>
-      ) : (
-        <button className="button-1" onClick={connectToMetaMask}>
-          Connect MetaMask
-        </button>
+    <div id="message">
+      {provider && (
+        <>
+          <h3 data-cy="title">MetaMask Detected</h3>
+          <p data-cy="address">Your address is: {address || "-"}</p>
+          <p data-cy="balance">Balance: {balance || "-"}</p>
+          <button data-cy="connect" className="button-1" onClick={connect}>
+            connect
+          </button>
+        </>
+      )}
+      {!provider && (
+        <h3 data-cy="title">
+          No MetaMask Detected - please install the extension!
+        </h3>
       )}
     </div>
   );
 }
-
-export default App;
